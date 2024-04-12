138d19cf978d50b096e150e11461c2b4e323deb9
Bronwen <bronwen@luxi.dev>
2024-04-12 13:34:46 +0100

    js fetch working

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ fetch('https://dev.io-academy.uk/resources/bond.php').then((response) => {
     return response.json()
 }).then((data) => {
     console.log(data)
+    
+    for(const d of data.data) {
+        document.querySelector('.output1').innerHTML += `<li>${d.name} (${d.release_date})</li>`
+    }
 });
